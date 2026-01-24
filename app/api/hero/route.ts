d94958257e1418aa344bc3687803466a1df56df8
codinister <codinister@gmail.com>
2026-01-24 16:11:54 +0100

    removed NextApiRequest type

--- app/api/hero/route.ts
+++ app/api/hero/route.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest } from 'next';
+
 import { NextResponse } from 'next/server';
 import { groq } from 'next-sanity';
 import clientConfig from '@/state/sanity/clientConfig';
@@ -6,7 +6,7 @@ import clientConfig from '@/state/sanity/clientConfig';
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
-export async function GET(req: NextApiRequest) {
+export async function GET() {
   try {
     const data = await clientConfig.fetch(groq`*[_type == 'ourMission']{
       content{
